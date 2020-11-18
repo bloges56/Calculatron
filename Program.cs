bb59c4c525fa483c96d4e8801da202790bff7aa2
bloges56 <brady.drew.logan@gmail.com>
2020-11-18 15:24:43 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Calculatron
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
